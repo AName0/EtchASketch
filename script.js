11ef420ea8298a21075cc98d0e96913697397c29
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-22 13:13:07 +0100

    Added IDs to rows

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ const container = document.querySelector(".container")
 function createRows(numOfRows) {
     for (let i = 1; i <= numOfRows; i++) {
         let row = document.createElement("div");
-        row.className = `row${i}`;
+        row.id = `row${i}`;
+        row.className = "row";
         container.appendChild(row);
     }
 }
 
-function createGrid(dimensions) { // TODO: display these divs as a grid
-    for (let i = 0; i < (dimensions ** 2); i++) {
-        let square = document.createElement("div");
-        container.appendChild(square);
-    }
+function createSquares(numOfSquares) {
+    const rows = document.querySelectorAll()
 }
+
+// function createGrid(dimensions) { // TODO: display these divs as a grid
+//     for (let i = 0; i < (dimensions ** 2); i++) {
+//         let square = document.createElement("div");
+//         container.appendChild(square);
+//     }
+// }
 
 createRows(16);
