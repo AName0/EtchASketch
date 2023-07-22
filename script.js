879b21ccd1809b2eb1ceee029a20896725032899
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-22 13:07:16 +0100

    function to create rows to store squares

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // script that will create a 16x16 grid of divs
 const container = document.querySelector(".container")
 
+function createRows(numOfRows) {
+    for (let i = 1; i <= numOfRows; i++) {
+        let row = document.createElement("div");
+        row.className = `row${i}`;
+        container.appendChild(row);
+    }
+}
+
 function createGrid(dimensions) { // TODO: display these divs as a grid
     for (let i = 0; i < (dimensions ** 2); i++) {
         let square = document.createElement("div");
@@ -8,4 +16,4 @@ function createGrid(dimensions) { // TODO: display these divs as a grid
     }
 }
 
-createGrid(16);
+createRows(16);
