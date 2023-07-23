4acc6ec9d0d462cffb56b608b61bbfe747011dbd
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-23 20:47:29 +0100

    createSquares now sets length of squares when called

--- script.js
+++ script.js
@@ -10,27 +10,33 @@ function createRows(numOfRows) {
     }
 }
 
-function createSquares() {
+function createSquares(gridSize) {
+    let length = 800/gridSize;
     const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
         let square = document.createElement("div");
         square.className = "square";
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "green"
+            square.style.backgroundColor = "green";
         })
+        square.style.height = length + "px";
+        square.style.width = length + "px";
         row.appendChild(square);
     })
 }
 
-function createGrid(num) {
-    createRows(num);
-    for (let i=1; i <= num; i++) {
-        createSquares();
+function createGrid(gridSize) {
+    createRows(gridSize);
+    for (let i=1; i <= gridSize; i++) {
+        createSquares(gridSize);
     }
 }
 
-function changeSquareColor() {
-    
+function changeGridSize() {
+    let gridSize = prompt("Type in a number from 4 to 100");
+    if (gridSize < 4 || gridSize > 100) {
+        return ("Error: Grid size invalid")
+    } else {
+        createGrid(gridSize)
+    }
 }
-
-createGrid(16);
